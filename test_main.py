20cc93da0940d699b42aa091d7cca0d2940a0e1f
Abu Afeez <abuafeezabolarinwa@yahoo.com>
2022-08-05 12:22:17 +0100

    update file

--- test_main.py
+++ test_main.py
@@ -7,8 +7,7 @@ import pytest
 
 import main
 
-SECRET = 'TestSecret'
-TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1NjEzMDY3OTAsIm5iZiI6MTU2MDA5NzE5MCwiZW1haWwiOiJ3b2xmQHRoZWRvb3IuY29tIn0.IpM4VMnqIgOoQeJxUbLT-cRcAjK41jronkVrqRLFmmk'
+
 EMAIL = 'wolf@thedoor.com'
 PASSWORD = 'huff-puff'
 
@@ -38,3 +37,14 @@ def test_auth(client):
     assert response.status_code == 200
     token = response.json['token']
     assert token is not None
+
+def test_400_sent_no_form_data_auth(client):
+    body = {'email': "",
+            'password': ""}
+    response = client.post('/auth', 
+                           data=json.dumps(body),
+                           content_type='application/json')
+
+    assert response.status_code == 400
+    token = response.json['token']
+    assert token is None
